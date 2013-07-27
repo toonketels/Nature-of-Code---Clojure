86690ea2f385c3c1b504a786f2b6a473f675f403
Toon Ketels <toon.ketels@wunderkraut.com>
2013-07-27 15:47:36 +0200

    Refactor walker ns.

--- projects/walker/src/walker/walker.clj
+++ projects/walker/src/walker/walker.clj
@@ -1,6 +1,39 @@
+; Walker
+;
+; Public api:
+;  - createWalker
+;  - walk
+;  - show
+;
+; @todo: Accept mayMakeChoise and choose as
+;        parameters to modify behavior at runtime 
 (ns walker.walker
   (:use quil.core))
 
+
+; Updates the coordinates and returns updated walker
+(defn- updateWalker [choice x-val y-val]
+  (case choice
+    0 (identity {:x (+ x-val 1) :y y-val :prev 0})
+    1 (identity {:x (- x-val 1) :y y-val :prev 1})
+    2 (identity {:x x-val :y (+ y-val 1) :prev 2})
+    3 (identity {:x x-val :y (- y-val 1) :prev 3})))
+
+
+; Can this option be chosen?
+; Modify this function to modify walker behavior
+(defn- mayMakeChoise? [walker choice]
+  (not= choice (get walker :prev)))
+
+
+; Choose an option (4 possibilities)
+; Modify this function to modify walker behavior
+(defn- choose [walker]
+  (int (random 4)))
+
+
+; Public api
+;-----------
 
 ; Creates a new walker data object which is a map
 (defn createWalker []
@@ -11,18 +44,14 @@
 
 ; Helper to update the coordinates
 (defn walk [walker]
-  (let [choice (int (random 4))
+  (let [choice (choose walker)
         x-val (get walker :x)
         y-val (get walker :y)]
     (cond
-      (= choice (get walker :prev)) (do 
-        (println "Nope, same as before, do it again") 
-        (walk walker))
-      :else (case choice
-          0 (identity {:x (+ x-val 1) :y y-val :prev 0})
-          1 (identity {:x (- x-val 1) :y y-val :prev 1})
-          2 (identity {:x x-val :y (+ y-val 1) :prev 2})
-          3 (identity {:x x-val :y (- y-val 1) :prev 3})))))
+      (mayMakeChoise? walker choice) (updateWalker choice x-val y-val) 
+      :else (do 
+        (println "Nope, we are not allowed to choose this. Choose again.") 
+        (walk walker)))))
 
 
 ; Display the walker
